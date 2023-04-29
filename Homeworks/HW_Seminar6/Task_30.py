af7aa38dcd3ba0442cfd1a37453e65498b6bed2e
Vadim Cheremnykh <chere96@yandex.ru>
2023-04-29 14:50:14 +0400

    changed file

--- Homeworks/HW_Seminar6/Task_30.py
+++ Homeworks/HW_Seminar6/Task_30.py
@@ -3,15 +3,15 @@
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
 
-# a = int(input("Enter the first element: "))
-# b = int(input("Enter the difference of the elements: "))
-# c = int(input("Enter the count of the elements: "))
-#
-# def result(a, b, c):
-#     my_list = []
-#     my_element = 0
-#     for i in range(1, c + 1):
-#         my_list.append(a + (i - 1) * b)
-#     print(my_list)
-#
-# result(a, b, c)
+a = int(input("Enter the first element: "))
+b = int(input("Enter the difference of the elements: "))
+c = int(input("Enter the count of the elements: "))
+
+def result(a, b, c):
+    my_list = []
+    my_element = 0
+    for i in range(1, c + 1):
+        my_list.append(a + (i - 1) * b)
+    print(my_list)
+
+result(a, b, c)
