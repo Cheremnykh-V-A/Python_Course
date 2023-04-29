f3030b637103c5479cccbb865d3d2bc9503f6499
Vadim Cheremnykh <chere96@yandex.ru>
2023-04-29 14:46:28 +0400

    added Task_32 file

---
+++ Homeworks/HW_Seminar6/Task_32.py
@@ -0,0 +1,22 @@
+# Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не
+# меньше заданного минимума и не больше заданного
+# максимума)
+# Ввод: [-5, 9, 0, 3, -1, -2, 1,
+# 4, -2, 10, 2, 0, -9, 8, 10, -9,
+# 0, -5, -5, 7]
+# Вывод: [1, 9, 13, 14, 19]
+
+# min_element = int(input("Enter the min element value: "))
+# max_element = int(input("Enter the max element value: "))
+#
+# def our_list(min_element, max_element):
+#     some_list1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+#     some_list2 = []
+#     for i in range(len(some_list1)):
+#         if min_element <= some_list1[i] <= max_element:
+#             some_list2.append(i)
+#     print(some_list2)
+#
+#
+# our_list(min_element, max_element)
